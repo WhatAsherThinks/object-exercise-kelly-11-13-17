ceb83fc578dd75dea211db02c9eaa09d31c3cb11
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-13 20:15:47 -0500

    adds readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Object Exercise
+This is an exercise in representing everyday things as an object. IN this example a plane object is created. 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-var planes: {
+var planes = {
   model: "Boeing 757",
   size: "Midsize",
   engine: 2,
-  color: ['yellow', 'white', 'blue'];
+  color: ['yellow', 'white', 'blue'],
   land: true,
   snack: {
     pretzels: false,
     peanuts: "salted",
     bread: false,
-    water: "tap" 
-  }
+    water: "tap"
+  },
   greeting: function (time, name) {
-    console.log("Good" + time + name);
+    console.log("Good" + " " + time + " " + name);
   }
-  plane.greeting('Afternoon', 'passengers')
-}
+};
+
+planes.greeting('Afternoon', 'passengers');
