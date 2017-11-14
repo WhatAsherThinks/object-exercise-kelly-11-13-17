24b639462375f335087fb64b226c760d3e73340b
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-13 19:04:46 -0500

    describes plane

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-
+var planes: {
+  model: "Boeing 757",
+  size: "Midsize",
+  engine: 2,
+  color: ['yellow', 'white', 'blue'];
+  land: true,
+  snack: {
+    pretzels: false,
+    peanuts: "salted",
+    bread: false,
+    water: "tap" 
+  }
+  greeting: function (time, name) {
+    console.log("Good" + time + name);
+  }
+  plane.greeting('Afternoon', 'passengers')
+}
